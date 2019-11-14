0664f19c5445ab51fdd659dc25c75571dfad6104
Aaron Adler <aaron.adler@revature.net>
2019-11-14 18:11:54 -0500

    OrdersModelComplete

---
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Models/OrdersModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChajdPizzaWebApp.Models
+{
+    public class OrdersModel
+    {
+        public int Customer_id { get; set; }
+        public decimal NetPrice { get; set; }
+        public bool isCompleted { get; set; }
+        public string DeliveryAddress { get; set; }
+
+    }
+}
