1fc1cf37b506fbb332a950746611f6a2700641d5
Dinesh Ghimire <dineshg822@gmail.com>
2019-11-14 14:59:48 -0500

    home page

---
+++ TestWebApp/TestWebApp/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public List<Customer> customers { get; set; }
+        public List<pizza> pizzas { get; set; }
+    }
+}
---
+++ TestWebApp/TestWebApp/Models/pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Models
+{
+    public class pizza
+    {
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public string Type { get; set; }
+        public decimal price { get; set; }
+        
+
+    }
+}
