ebe05f005a8a6766ecde429cb9d4c6b37bc2080e
Aaron Adler <aaron.adler@revature.net>
2019-11-14 18:43:08 -0500

    DbContextOrdersModel

--- ChajdPizzaWebApp/ChajdPizzaWebApp/Data/ApplicationDbContext.cs
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ChajdPizzaWebApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace ChajdPizzaWebApp.Data
             : base(options)
         {
         }
+
+        DbSet<OrdersModel> Orders { get; set; }
     }
 }
