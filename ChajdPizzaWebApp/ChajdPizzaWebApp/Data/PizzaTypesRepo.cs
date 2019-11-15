5cc11a858a4b4ff5ab7df567f86170cff7c0412a
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-15 10:53:31 -0500

    Added in accessors for getting Secret Formulas.

--- ChajdPizzaWebApp/ChajdPizzaWebApp/Data/PizzaTypesRepo.cs
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Data/PizzaTypesRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChajdPizzaWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,5 +20,39 @@ namespace ChajdPizzaWebApp.Data
             _repo = repo;
         }
 
+
+        public IEnumerable<SecretFormula> GetSecretFormulas()
+        {
+            IEnumerable<SecretFormula> result = null;
+
+            var query = _repo.SecretFormula.Where(c => c.Id == c.Id);
+            if (query.Count()>0)
+            {
+                result = query;
+            }
+            else
+            {
+                throw new NullReferenceException("EMPTY QUERY IN SECRET FORMULA!");
+            }
+
+            return result;
+        }
+
+        public SecretFormula GetSecretFormula(int id)
+        {
+            SecretFormula result = null;
+
+            var query = _repo.SecretFormula.Where(c => c.Id == c.Id);
+            if (query.Count() > 0)
+            {
+                result = query.FirstOrDefault();
+            }
+            else
+            {
+                throw new NullReferenceException("EMPTY QUERY IN SECRET FORMULA!");
+            }
+
+            return result;
+        }
     }
 }
