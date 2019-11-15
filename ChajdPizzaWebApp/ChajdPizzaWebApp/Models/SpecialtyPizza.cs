b8d1859ceae6494262c89cf219500b465b3f88d2
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-15 09:24:51 -0500

    Created new property class for SpecialtyPizza table and data objects.

---
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Models/SpecialtyPizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChajdPizzaWebApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Customer_id { get; set; }
+        public decimal NetPrice { get; set; }
+        [Required]
+        public bool isCompleted { get; set; }
+        public string DeliveryAddress { get; set; }
+
+    }
+}
