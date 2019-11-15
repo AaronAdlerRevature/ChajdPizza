1621d18b9f0032e3a96ec26232c8e0c1737ef419
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-15 11:14:19 -0500

    Setup seeding for PizzaTypeDbContext.

--- ChajdPizzaWebApp/ChajdPizzaWebApp/Data/PizzaTypeDbContext.cs
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Data/PizzaTypeDbContext.cs
@@ -20,6 +20,19 @@ namespace ChajdPizzaWebApp.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<SecretFormula>().HasData(new SecretFormula() { Id = 1, Price = 1.0M });
+            
+            //modelBuilder.Entity<Size>().HasData(new Size() { Id = 1, BaseSize = "Small", S_Price = 3.0M });
+            //modelBuilder.Entity<Size>().HasData(new Size() { Id = 2, BaseSize = "Medium", S_Price = 5.0M });
+            //modelBuilder.Entity<Size>().HasData(new Size() { Id = 3, BaseSize = "Large", S_Price = 7.0M });
+
+            //modelBuilder.Entity<SpecialtyPizza>().HasData(new SpecialtyPizza() { ID = 1, Name = "Special", Description = "Special Pizza", Price = 10.0 });
+
+            //modelBuilder.Entity<Toppings>().HasData(new Toppings() { Id = 1, Name = "Pepperoni" });
+        }
+
         public DbSet<SecretFormula> SecretFormula { get; set; }
         public DbSet<Size> Size { get; set; }
         public DbSet<SpecialtyPizza> SpecialtyPizzas { get; set; }
