fe877c1df18d3393ccbfd5683aa7f70ac71f250e
Aaron Adler <aaron.adler@revature.net>
2019-11-15 10:12:26 -0500

    Models for SF, Size, Toppings

---
+++ ChajdPizzaWebApp/ChajdPizzaWebApp/Models/Size.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChajdPizzaWebApp.Models
+{
+    public class Size
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string BaseSize { get; set; }
+        [Required, Column(TypeName = "decimal(18,2)")]
+        public decimal S_Price { get; set; }
+    }
+}
