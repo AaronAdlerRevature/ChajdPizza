2ee6b31144bbcf8098485eab4267612273c0a9ad
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-18 14:43:37 -0500

    Created new unit test class for CustomersAPI controller.

---
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_CustomersApiController.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Test_CustomersApiController
+    {
+        [TestMethod]
+        public void Get()
+        {
+            #region ASSIGN
+
+            #endregion
+
+            #region ACT
+
+            #endregion
+
+            #region ASSERT
+
+            #endregion
+
+        }
+    }
+}
