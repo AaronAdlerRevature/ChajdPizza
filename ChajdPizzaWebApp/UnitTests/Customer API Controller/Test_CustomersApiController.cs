0cf65d73ae801a33ca8383daa07ced29045e7c01
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-18 14:54:38 -0500

    Created unit test for GetCustomers action of CustomersAPI controller.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_CustomersApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_CustomersApiController.cs
@@ -1,4 +1,8 @@
+using ChajdPizzaWebApp.Controllers;
+using ChajdPizzaWebApp.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using UnitTests.Data_Objects;
 
 namespace UnitTests
 {
@@ -6,17 +10,29 @@ namespace UnitTests
     public class Test_CustomersApiController
     {
         [TestMethod]
-        public void Get()
+        public void GetCustomers()
         {
             #region ASSIGN
+
+            CustomerRepo testRepo = new CustomerRepo();
+            CustomersApiController testController = new CustomersApiController(testRepo);
 
             #endregion
 
             #region ACT
 
+            var taskReturn = testController.GetCustomer();
+            taskReturn.Wait();
+            var result = taskReturn.Result.Value;
+
+            List<Customer> testList = new List<Customer>(result);
+
             #endregion
 
             #region ASSERT
+
+            Assert.AreEqual(testList.Count, 2);
+            Assert.AreEqual(testList[0].Name, "John Doe");
 
             #endregion
 
