71bab5c54f5c7f3e8a90635037610a10b6ca0c14
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-19 14:47:59 -0500

    Created new unit test class for testing OrdersDetailsApiController.

---
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_OrderDetailssApiController.cs
@@ -0,0 +1,43 @@
+using ChajdPizzaWebApp.Controllers;
+using ChajdPizzaWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using UnitTests.Data_Objects;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Test_OrderDetailsApiController
+    {
+        [TestMethod]
+        public void GetCustomers()
+        {
+            #region ASSIGN
+
+            CustomerRepo testRepo = new CustomerRepo();
+            CustomersApiController testController = new CustomersApiController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetCustomer();
+            taskReturn.Wait();
+            var result = taskReturn.Result.Value;
+
+            List<Customer> testList = new List<Customer>(result);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.AreEqual(testList.Count, 2);
+            Assert.AreEqual(testList[0].Name, "John Doe");
+
+            #endregion
+        }
+
+      
+    }
+}
