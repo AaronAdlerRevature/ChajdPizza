30a65cb08e080c11ac8a6c1841540d44e2a1c0b2
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-19 14:44:56 -0500

    Created new fake OrderDetailsRepo for Unit testing.

---
+++ ChajdPizzaWebApp/UnitTests/Data Objects/OrderDetailsRepo.cs
@@ -0,0 +1,47 @@
+﻿using ChajdPizzaWebApp.Models;
+using ChajdPizzaWebApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Data_Objects
+{
+    class OrderDetailsRepo : IOrderDetailsRepo
+    {
+        public Task<bool> Add(OrderDetail orderDetail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OrderDetailExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(OrderDetail orderDetail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderDetail>> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDetail> SelectById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderDetail>> SelectOrderAllDetails(int? orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(OrderDetail orderDetail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
