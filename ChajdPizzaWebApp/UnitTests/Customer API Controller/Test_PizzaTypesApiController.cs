3aef7675ec2f29a4c1052aef0b42b9d54d96f1ee
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 16:57:21 -0500

    Created unit test for getting description of specific specialty pizza, non-existing ID.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
@@ -661,5 +661,31 @@ namespace UnitTests
 
             #endregion
         }
+
+        [TestMethod]
+        public void GetSpecialtyPizzaDescription_NonExistingID()
+        {
+            #region ASSIGN
+
+            PizzaTypesRepo testRepo = new PizzaTypesRepo();
+            PizzaTypesAPIController testController = new PizzaTypesAPIController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetSpecialtyPizzaDescription(0);
+            taskReturn.Wait();
+            var result = taskReturn.Result.Result;
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual((result as NotFoundResult).StatusCode, 404);
+
+            #endregion
+        }
     }
 }
