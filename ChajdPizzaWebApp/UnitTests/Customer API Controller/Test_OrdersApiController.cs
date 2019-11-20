82e5073dc0690033e8dc389ac95bc1ee727d9527
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-19 20:04:07 -0500

    Created new unit test class for OrdersApiController.

---
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_OrdersApiController.cs
@@ -0,0 +1,15 @@
+using ChajdPizzaWebApp.Controllers;
+using ChajdPizzaWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using UnitTests.Data_Objects;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Test_OrdersApiController
+    {
+       
+    }
+}
