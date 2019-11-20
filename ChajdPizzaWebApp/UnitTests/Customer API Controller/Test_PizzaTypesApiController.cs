54d782a7c677e949e269c29787e23f257ef59693
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 09:39:21 -0500

    Created new unit test class for testing PizzaTypesApiController.

---
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
@@ -0,0 +1,16 @@
+using ChajdPizzaWebApp.Controllers;
+using ChajdPizzaWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using UnitTests.Data_Objects;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Test_PizzaTypesApiController
+    {
+       
+    }
+}
