ca56ef2b6e8fad281b5d9e9617854c16109d8713
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 16:37:59 -0500

    Created new unit test for getting all specialty pizzas, empty data list.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
@@ -362,5 +362,31 @@ namespace UnitTests
 
             #endregion
         }
+
+        [TestMethod]
+        public void GetSpecialtyPizzas_EmptyDataList()
+        {
+            #region ASSIGN
+
+            PizzaTypesRepo testRepo = new PizzaTypesRepo(false);
+            PizzaTypesAPIController testController = new PizzaTypesAPIController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetSpecialtyPizzas();
+            taskReturn.Wait();
+            var result = taskReturn.Result.Result;
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual((result as NotFoundResult).StatusCode, 404);
+
+            #endregion
+        }
     }
 }
