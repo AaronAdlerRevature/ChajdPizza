74bc6699299c2f1321fc1f623c048fa708b9f132
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 09:51:35 -0500

    Created new test data repository for PizzaTypesRepo.

---
+++ ChajdPizzaWebApp/UnitTests/Data Objects/PizzaTypesRepo.cs
@@ -0,0 +1,95 @@
+﻿using ChajdPizzaWebApp.Models;
+using ChajdPizzaWebApp.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Data_Objects
+{
+    class PizzaTypesRepo : IPizzaTypesRepo
+    {
+
+        public PizzaTypesRepo()
+        {
+
+        }
+
+        public Task<Size> GetPizzaSize(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPizzaSizeName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> GetPizzaSizePrice(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Size>> GetPizzaSizes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SecretFormula> GetSecretFormula(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> GetSecretFormulaPrice(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SecretFormula>> GetSecretFormulas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SpecialtyPizza> GetSpecialtyPizza(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetSpecialtyPizzaDescription(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetSpecialtyPizzaName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> GetSpecialtyPizzaPrice(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SpecialtyPizza>> GetSpecialtyPizzas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Toppings> GetTopping(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetToppingName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Toppings>> GetToppings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
