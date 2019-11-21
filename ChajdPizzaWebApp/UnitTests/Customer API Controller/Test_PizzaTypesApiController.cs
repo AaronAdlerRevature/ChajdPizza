8543fb39d4d3a28747af4455bcc7344f7b9f9b5e
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 21:04:20 -0500

    Created unit test for getting secret formula price, non-existing ID.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
@@ -1115,5 +1115,31 @@ namespace UnitTests
 
             #endregion
         }
+
+        [TestMethod]
+        public void GetSecretFormulaPrice_NonExistingID()
+        {
+            #region ASSIGN
+
+            PizzaTypesRepo testRepo = new PizzaTypesRepo();
+            PizzaTypesAPIController testController = new PizzaTypesAPIController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetSecretFormula(0);
+            taskReturn.Wait();
+            var result = taskReturn.Result.Result;
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual((result as NotFoundResult).StatusCode, 404);
+
+            #endregion
+        }
     }
 }
