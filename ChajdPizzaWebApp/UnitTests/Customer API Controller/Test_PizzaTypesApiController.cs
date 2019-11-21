88ce8053860abf63920a9139e1f85f44f2f4509d
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-20 20:35:37 -0500

    Created unit test for getting specific topping, non-existing ID.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_PizzaTypesApiController.cs
@@ -811,5 +811,31 @@ namespace UnitTests
 
             #endregion
         }
+
+        [TestMethod]
+        public void GetTopping_NonExistingID()
+        {
+            #region ASSIGN
+
+            PizzaTypesRepo testRepo = new PizzaTypesRepo();
+            PizzaTypesAPIController testController = new PizzaTypesAPIController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetTopping(0);
+            taskReturn.Wait();
+            var result = taskReturn.Result.Result;
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual((result as NotFoundResult).StatusCode, 404);
+
+            #endregion
+        }
     }
 }
