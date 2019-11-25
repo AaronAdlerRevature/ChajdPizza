abe7be8ef2d828956ea4aedca9e71ca00b245c75
Cameron Marion <cameron.marion@revature.net>
2019-11-25 09:54:03 -0500

    Created default test data for StateRepo class.

--- ChajdPizzaWebApp/UnitTests/Data Objects/StateRepo.cs
+++ ChajdPizzaWebApp/UnitTests/Data Objects/StateRepo.cs
@@ -9,6 +9,39 @@ namespace UnitTests.Data_Objects
 {
     class StateRepo : IStateRepo
     {
+        List<State> allStates = null;
+
+
+        StateRepo()
+        {
+            allStates = new List<State>();
+
+            allStates.Add(new State()
+            {
+                ID = 1,
+                Name = "Alaska",
+                Abbreviation="AK",
+            }); 
+            allStates.Add(new State()
+            {
+                ID = 2,
+                Name = "Virgina",
+                Abbreviation="VA",
+            }); 
+            allStates.Add(new State()
+            {
+                ID = 3,
+                Name = "Florida",
+                Abbreviation="FL",
+            }); 
+            allStates.Add(new State()
+            {
+                ID = 4,
+                Name = "Texas",
+                Abbreviation="TX",
+            }); 
+        }
+
         public Task<State> GetState(int id)
         {
             throw new NotImplementedException();
