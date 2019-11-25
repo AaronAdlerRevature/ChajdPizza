e75deee74c82760a9c91d5af1a705947cf4ec6f6
Cameron Marion <cameron.marion@revature.net>
2019-11-25 10:07:08 -0500

    Created unit test for getting state name by ID, non-existing ID.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_StateApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_StateApiController.cs
@@ -189,5 +189,31 @@ namespace UnitTests
 
             #endregion
         }
+
+        [TestMethod]
+        public void GetStateName_NonExistingID()
+        {
+            #region ASSIGN
+
+            StateRepo testRepo = new StateRepo();
+            StateApiController testController = new StateApiController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetStateName(0);
+            taskReturn.Wait();
+            var result = taskReturn.Result.Result;
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual((result as NotFoundResult).StatusCode, 404);
+
+            #endregion
+        }
     }
 }
