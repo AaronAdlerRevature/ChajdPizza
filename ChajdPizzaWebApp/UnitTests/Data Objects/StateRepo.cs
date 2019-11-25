530e2e92b8a7cd934ec6311954a6145d821dcf37
Cameron Marion <cameron.marion@revature.net>
2019-11-25 09:49:50 -0500

    Created new repo for unit testing StateApiController.

---
+++ ChajdPizzaWebApp/UnitTests/Data Objects/StateRepo.cs
@@ -0,0 +1,32 @@
+﻿using ChajdPizzaWebApp.Models;
+using ChajdPizzaWebApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Data_Objects
+{
+    class StateRepo : IStateRepo
+    {
+        public Task<State> GetState(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStateAbbrevation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStateName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<State>> GetStates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
