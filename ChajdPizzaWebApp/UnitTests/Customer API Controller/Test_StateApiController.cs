f19da06054f9f4dbd9130388ff2ef9acf205d23c
Cameron Marion <cameron.marion@revature.net>
2019-11-25 10:00:08 -0500

    Created unit test for getting all states, valid data.

--- ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_StateApiController.cs
+++ ChajdPizzaWebApp/UnitTests/Customer API Controller/Test_StateApiController.cs
@@ -10,6 +10,47 @@ namespace UnitTests
     [TestClass]
     public class Test_StateApiController
     {
-       
+        [TestMethod]
+        public void GetStates()
+        {
+            #region ASSIGN
+
+            StateRepo testRepo = new StateRepo();
+            StateApiController testController = new StateApiController(testRepo);
+
+            #endregion
+
+            #region ACT
+
+            var taskReturn = testController.GetStates();
+            taskReturn.Wait();
+            var result = taskReturn.Result.Value;
+
+            List<State> testList = new List<State>(result);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.AreEqual(testList.Count, 4);
+
+            Assert.AreEqual(testList[0].ID, 1);
+            Assert.AreEqual(testList[0].Name, "Alaska");
+            Assert.AreEqual(testList[0].Abbreviation, "AK");
+
+            Assert.AreEqual(testList[1].ID, 2);
+            Assert.AreEqual(testList[1].Name, "Virgina");
+            Assert.AreEqual(testList[1].Abbreviation, "VA");
+
+            Assert.AreEqual(testList[2].ID, 3);
+            Assert.AreEqual(testList[2].Name, "Florida");
+            Assert.AreEqual(testList[2].Abbreviation, "FL");
+
+            Assert.AreEqual(testList[3].ID, 4);
+            Assert.AreEqual(testList[3].Name, "Texas");
+            Assert.AreEqual(testList[3].Abbreviation, "TX");
+
+            #endregion
+        }
     }
 }
